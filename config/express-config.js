39aafecc0abfe6674c59ae9585abade38743430a
Italo Soares <irs@cin.ufpe.br>
2018-08-29 16:50:53 -0300

    Implementação de persistência e testes antes de salvar

--- config/express-config.js
+++ config/express-config.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var consign = require('consign')();
 var bodyParser = require('body-parser');
+var express_validator = require('express-validator');
 
 module.exports=function(){
     var app = express();
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
-    consign.include('routes').into(app);
+    app.use(express_validator());
+    consign.include('routes').then('persistencia').into(app);
     return app;
 };
 
--- routes/pagamentos.js
+++ routes/pagamentos.js
@@ -5,8 +5,21 @@ module.exports = function(app){
     });
 
     app.post('/pagamentos/pagamento', function(req, res){
+        req.assert("forma_de_pagamento", "Forma de pagamento é obrigatório").notEmpty();
+        req.assert("valor", "Preencha um valor válido").notEmpty().isFloat();
+        if (req.validationErrors()){
+            console.log("Erros na requisição");
+            res.status(400).send(req.validationErrors());
+            return;
+        }
         var pagamento = req.body;
-        console.log(pagamento);
-        res.send("Recebi seu pagamento");
+        pagamento.status="recebido";
+        pagamento.data= new Date;
+        var DBconnection = app.persistencia.connectionFactory();
+        var pagamentoDAO = new app.persistencia.PagamentoDAO(DBconnection);
+        pagamentoDAO.salva(pagamento, function(err, result){
+            if(err) res.send(err);
+            res.json("Recebi seu pagamento");
+        });
     });
 }
