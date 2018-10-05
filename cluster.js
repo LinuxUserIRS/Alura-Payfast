d369d51bc0a620922e61133fda5f26ceb3fcddfa
Viitra 2 <irs@cin.ufpe.br>
2018-10-05 15:43:13 -0300

    Deixando a clusterização mais inteligente

--- cluster.js
+++ cluster.js
@@ -1,9 +1,18 @@
 var cluster=require('cluster');
 var os=require('os');
+var logger=require('./servicos/logger.js');
 
 if(cluster.isMaster){
     os.cpus().forEach(function(){
         console.log("Fokando thread");
+        cluster.fork();
+    });
+    cluster.on('listening', function(worker){
+        logger.info('Thread ' + worker.process.pid +' iniciada');
+    });
+
+    cluster.on('exit', worker => {
+        logger.info('Thread '+worker.process.pid+' encerrada');
         cluster.fork();
     });
 }else{
